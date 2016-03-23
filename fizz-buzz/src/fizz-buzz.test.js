f6fa06adfc319bc076e191eadaea9bc16010d6c2
Juha Vainikka <vainikkaj@gmail.com>
2016-03-23 19:43:05 +0200

    Imported to version control

---
+++ fizz-buzz/src/fizz-buzz.test.js
@@ -0,0 +1,5 @@
+var module = require('./fizz-buzz');
+
+describe('fizz-buzz', function(){
+  throw new Error('Nothing here but us fails');
+});
---
+++ stringcalc/src/string-calculator.test.js
@@ -0,0 +1,5 @@
+var module = require('./string-calculator');
+
+describe('string-calculator', function(){
+  throw new Error('Nothing here but us fails');
+});
